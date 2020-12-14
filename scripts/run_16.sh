55f58430301ffa7d92bb1c33c3807a03a7162d83
Greendor1234 <62668708+Greendor1234@users.noreply.github.com>
2020-12-14 15:18:17 +0100

    Delete run_16.sh

--- scripts/run_16.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./bin/sudoku_crook_serial ./tests/matr_16_16_1.txt > ./output/matr_16_16_1.txt
-#./bin/sudoku_crook_serial ./tests/matr_16_16_2.txt > ./output/matr_16_16_2.txt
-./bin/sudoku_crook_serial ./tests/matr_16_16_3.txt > ./output/matr_16_16_3.txt
