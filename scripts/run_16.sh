57096b2d2b5cc40679a6c2120cf28f901eb6e05b
Greendor1234 <62668708+Greendor1234@users.noreply.github.com>
2020-12-14 14:12:12 +0100

    Add files via upload

---
+++ scripts/run_16.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+./sudoku_crook_copia ./input/matr_16_16_1.txt > ./output/matr_16_16_1.txt
+./sudoku_crook_copia ./input/matr_16_16_2.txt > ./output/matr_16_16_2.txt
+./sudoku_crook_copia ./input/matr_16_16_3.txt > ./output/matr_16_16_3.txt
---
+++ scripts/run_9.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+./sudoku_crook_copia ./input/matr_9_9_1.txt > ./output/matr_9_9_1.txt
+./sudoku_crook_copia ./input/matr_9_9_2.txt > ./output/matr_9_9_2.txt
+./sudoku_crook_copia ./input/matr_9_9_3.txt > ./output/matr_9_9_3.txt
