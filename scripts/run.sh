50440936399213e22959f16e12f4ef85763aabb7
Greendor1234 <62668708+Greendor1234@users.noreply.github.com>
2020-12-14 15:18:55 +0100

    Update and rename run_9.sh to run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+./bin/sudoku_crook_serial ./tests/matr_9_9_1.txt > ./output/matr_9_9_1.txt
+./bin/sudoku_crook_serial ./tests/matr_9_9_2.txt > ./output/matr_9_9_2.txt
+./bin/sudoku_crook_serial ./tests/matr_9_9_3.txt > ./output/matr_9_9_3.txt
+./bin/sudoku_crook_serial ./tests/matr_16_16_1.txt > ./output/matr_16_16_1.txt
+#./bin/sudoku_crook_serial ./tests/matr_16_16_2.txt > ./output/matr_16_16_2.txt
+./bin/sudoku_crook_serial ./tests/matr_16_16_3.txt > ./output/matr_16_16_3.txt
+./bin/sudoku_crook_serial ./tests/matr_16_16_4.txt > ./output/matr_16_16_4.txt
